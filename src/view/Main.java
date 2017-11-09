8dbcdd6474d844ab89fe811fc7f5f0f52e8f8b82
raquel <raquel@20.20.1.65>
2017-11-09 15:29:35 -0200

    primeiro comit

---
+++ src/view/Main.java
@@ -0,0 +1,10 @@
+
+package view;
+
+public class Main {
+    public static void Main(String args[] ){
+    TelaLogin tl = new TelaLogin();
+        tl.setVisible(true);
+        tl.setLocationRelativeTo(null);  
+    }
+}
